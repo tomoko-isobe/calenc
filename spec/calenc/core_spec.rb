09dd4fd0c632cfdce042b9bd75ecc8e21d1e20c4
磯部朋子 <isobe@humancrest.co.jp>
2018-11-28 14:16:52 +0900

    Don't repeat yourself

--- spec/calenc/core_spec.rb
+++ spec/calenc/core_spec.rb
@@ -12,30 +12,33 @@ describe ::Calenc::Core do
   end
 
   describe '#to_s' do
+    let(:str){ Calenc::Core.new(*params).to_s }
+    let(:date){ Date.new(2018,11,29) }
+
     context 'パラメータを指定しない場合' do
+      let(:params){ nil }
+
       it 'return something' do
-        str= Calenc::Core.new.to_s
         expect(str).not_to be_nil
       end
 
       it 'is a kind of String class' do
-        str= Calenc::Core.new.to_s
         expect(str).to be_a(String)
       end
     end
 
     context '日付を指定した場合' do
+      let(:params){ [7, date] }
+
       it '指定した日付から一週間分の日付の文字列を返すこと' do
-        date= Date.new(2018,11,29)
-        str= Calenc::Core.new(7, date).to_s
         expect(str).to eq "11/29(木)\n11/30(金)\n12/01(土)\n12/02(日)\n12/03(月)\n12/04(火)\n12/05(水)\n"
       end
     end
 
     context '日数を指定した場合' do
+      let(:params){ [3, date] }
+
       it '指定した日数分の日付の文字列を返すこと' do
-        date= Date.new(2018,11,29)
-        str= Calenc::Core.new(3, date).to_s
         expect(str).to eq "11/29(木)\n11/30(金)\n12/01(土)\n"
       end
     end
