036b090ec60b5b7a890ab70af6dc636c017291d9
磯部朋子 <isobe@humancrest.co.jp>
2018-11-28 12:21:13 +0900

    Add spec #to_s but it is not good spec

--- spec/calenc/core_spec.rb
+++ spec/calenc/core_spec.rb
@@ -21,5 +21,10 @@ describe ::Calenc::Core do
       str= Calenc::Core.new.to_s
       expect(str).to be_a(String)
     end
+
+    it '一週間分の日付の文字列を返すこと' do
+      str= Calenc::Core.new.to_s
+      expect(str).to eq "11/29(木)\n11/30(金)\n12/01(土)\n12/02(日)\n12/03(月)\n12/04(火)\n12/05(水)\n"
+    end
   end
 end
