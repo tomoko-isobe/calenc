a495a9cbb3bef53a89cdd3b94a48878bc4edda1a
磯部朋子 <isobe@humancrest.co.jp>
2018-12-05 10:22:28 +0900

    Rewrite specs from clipboard to standard output

--- spec/calenc/command_spec.rb
+++ spec/calenc/command_spec.rb
@@ -1,5 +1,25 @@
 describe ::Calenc::Command do
-  it 'clip some string' do
-    expect{ Calenc::Command.run }.not_to raise_error(StandardError)
+  context 'No command line option' do
+    it 'output some string' do
+      expect{ Calenc::Command.run }.to output(be_a_kind_of(String)).to_stdout
+    end
+
+    it 'output date string' do
+      exp= /\d\d\/\d\d\(.\)\n/
+      expect{ Calenc::Command.run }.to output(match(exp)).to_stdout
+    end
+
+    it 'output 7 days' do
+      expect{ Calenc::Command.run }.to output(satisfy{|s| s.split("\n").count== 7 }).to_stdout
+    end
+  end
+
+  context 'With command line option' do
+    it 'days count' do
+      ARGV.clear
+      ARGV.concat(["--count", "3"])
+      expect(Calenc::Core).to receive(:new).with(3)
+      Calenc::Command.run
+    end
   end
 end
